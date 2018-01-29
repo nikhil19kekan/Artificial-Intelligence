be3f124f97c8c208911278ce86d5e46275129155
nikhilkumar kekan <nikhil19kekan@gmail.com>
2018-01-28 19:34:49 -0600

    AI assignment 1

---
+++ Assignment1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+def readFileLine(sourceCity):
+	f=open("C:\Users\Nikhil\PycharmProjects\AI1\AI-ass1\input1.txt","r")
+	for line in f:
+		print(line)
+	f.close()
+readFileLine("hi")
+l=[]
+l.sort()
